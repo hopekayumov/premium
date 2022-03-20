2712819966f27bdad908be06e224a80af368fba7
HopeKayumov <umidjonkayumov2000@gmail.com>
2022-03-20 16:44:35 +0500

    add images

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { images } from "../../constants";
 
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul>
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li key={item}>
+            <div>
+              <a href={`#${item}`}>{item}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
