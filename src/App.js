96d7b64c765d0032d9a14d4c5432314473ff7ac6
HopeKayumov <umidjonkayumov2000@gmail.com>
2022-03-20 16:35:34 +0500

    add pages and some styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+// App styles
+import "./App.scss";
+
+// Containers
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
+// Components
+import { Navbar } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/container/index.js
@@ -0,0 +1,12 @@
+// About page
+export { default as About } from "./About/About";
+// Footer page
+export { default as Footer } from "./Footer/Footer";
+// Header page
+export { default as Header } from "./Header/Header";
+// Skills page
+export { default as Skills } from "./Skills/Skills";
+// Testimonials page
+export { default as Testimonial } from "./Testimonial/Testimonial";
+// Work page
+export { default as Work } from "./Work/Work";
